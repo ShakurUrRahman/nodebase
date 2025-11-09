3891475b993550e345c8953bc1beb8cfe0a25c83
ShakurUrRahman <shakururrahman@gmail.com>
2025-11-09 21:44:19 +0600

    06 background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,33 @@
+"use client";
+
 import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
 import { LogoutButton } from "./logout";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+import { use } from "react";
 
-export default async function Home() {
-	await requireAuth();
+export default function Home() {
+	const trpc = useTRPC();
+	const queryClient = useQueryClient();
 
-	const data = await caller.getUsers();
+	const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+	const create = useMutation(
+		trpc.createWorkflow.mutationOptions({
+			onSuccess: () =>
+				queryClient.invalidateQueries(trpc.getWorkflows.queryOptions()),
+		})
+	);
 
 	return (
 		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 			protected
 			{JSON.stringify(data)}
+			<Button disabled={create.isPending} onClick={() => create.mutate()}>
+				Create Workflow
+			</Button>
 			<LogoutButton />
 		</div>
 	);
