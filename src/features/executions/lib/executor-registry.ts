228e84e9e6a1cd1c226020b975a3b590a1983f4c
ShakurUrRahman <shakururrahman@gmail.com>
2025-11-24 20:22:45 +0600

    22-google-form-trigger

--- src/features/executions/lib/executor-registry.ts
+++ src/features/executions/lib/executor-registry.ts
@@ -2,11 +2,13 @@ import { NodeType } from "@/generated/prisma/enums";
 import { NodeExecutor } from "../types";
 import { manualTriggerExecutor } from "@/features/trigger/components/manual-trigger/executor";
 import { httpRequestExecutor } from "../components/http-request/executor";
+import { googleFormTriggerExecutor } from "@/features/trigger/components/google-form-trigger/executor";
 
 export const executorRegistry: Record<NodeType, NodeExecutor> = {
 	[NodeType.INITIAL]: manualTriggerExecutor,
 	[NodeType.MANUAL_TRIGGER]: manualTriggerExecutor,
 	[NodeType.HTTP_REQUEST]: httpRequestExecutor,
+	[NodeType.GOOGLE_FORM_TRIGGER]: googleFormTriggerExecutor,
 };
 
 export const getExecutor = (type: NodeType): NodeExecutor => {
---
+++ src/features/trigger/components/google-form-trigger/node.tsx
@@ -0,0 +1,39 @@
+import { memo, useState } from "react";
+import type { NodeProps } from "@xyflow/react";
+import { MousePointerIcon } from "lucide-react";
+import { BaseTriggerNode } from "../base-trigger-node";
+import { GoogleFormTriggerDialog } from "./dialog";
+import { fetchGoogleFormTriggerRealtimeToken } from "./action";
+import { useNodeStatus } from "@/features/executions/hooks/use-node-status";
+import { GOOGLE_FORM_TRIGGER_CHANNEL_NAME } from "@/inngest/channels/google-form-trigger";
+
+export const GoogleFormTrigger = memo((props: NodeProps) => {
+	const [dialogOpen, setDialogOpen] = useState(false);
+
+	const nodeStatus = useNodeStatus({
+		nodeId: props.id,
+		channel: GOOGLE_FORM_TRIGGER_CHANNEL_NAME,
+		topic: "status",
+		refreshToken: fetchGoogleFormTriggerRealtimeToken,
+	});
+
+	const handleOpenSettings = () => setDialogOpen(true);
+
+	return (
+		<>
+			<GoogleFormTriggerDialog
+				open={dialogOpen}
+				onOpenChange={setDialogOpen}
+			/>
+			<BaseTriggerNode
+				{...props}
+				icon={"/logos/googleform.svg"}
+				name="Google Form"
+				description="When fom is submitted"
+				status={nodeStatus}
+				onSettings={handleOpenSettings}
+				onDoubleClick={handleOpenSettings}
+			/>
+		</>
+	);
+});
