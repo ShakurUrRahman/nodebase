091c36e0277e4d9f9b1d32a8f2a5f0b1d2b1c764
ShakurUrRahman <shakururrahman@gmail.com>
2025-12-08 17:17:47 +0600

    type-ignore

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	devIndicators: false,
-	typescript: {
-		ignoreBuildErrors: true,
-	},
+	// typescript: {
+	// 	ignoreBuildErrors: true,
+	// },
 	async redirects() {
 		return [
 			{
