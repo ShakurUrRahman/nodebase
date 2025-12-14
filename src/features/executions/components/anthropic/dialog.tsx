60e2efa4445bb6c143defdb8fabbb463c6732874
ShakurUrRahman <shakururrahman@gmail.com>
2025-12-14 18:43:07 +0600

    sorted-things

--- src/features/executions/components/anthropic/dialog.tsx
+++ src/features/executions/components/anthropic/dialog.tsx
@@ -34,7 +34,7 @@ import {
 	SelectValue,
 } from "@/components/ui/select";
 import { useCredentialsByType } from "@/features/credentials/hooks/use-credentials";
-import { type CredentialType } from "@/generated/prisma/enums";
+import { CredentialType } from "@/generated/prisma/enums";
 
 export const formSchema = z.object({
 	variableName: z
