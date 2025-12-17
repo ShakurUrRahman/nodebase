979bdac71e1657955ac2b770bb64807b35d523a0
ShakurUrRahman <shakururrahman@gmail.com>
2025-12-17 18:55:51 +0600

    error-boundary

--- src/lib/auth-utils.ts
+++ src/lib/auth-utils.ts
@@ -3,13 +3,20 @@ import { redirect } from "next/navigation";
 import { auth } from "./auth";
 
 export const requireAuth = async () => {
-	const session = await auth.api.getSession({
-		headers: await headers(),
-	});
-	if (!session) {
+	try {
+		const session = await auth.api.getSession({
+			headers: await headers(),
+		});
+
+		if (!session) {
+			redirect("/login");
+		}
+
+		return session;
+	} catch (err) {
+		// ⬇️ PROD-এ crash না করে redirect
 		redirect("/login");
 	}
-	return session;
 };
 
 export const requireUnauth = async () => {
